cf0b6e1e94b82582416f9594c2a58bb664df6d5c
Lautaro <lautaro@vekto.dev>
2026-03-13 01:06:49 -0300

    fix homepage redirect

--- smile-landing/app/page.tsx
+++ smile-landing/app/page.tsx
@@ -1,15 +1,5 @@
-import Header from "../components/layout/Header";
-import Hero from "../components/sections/Hero";
-import Collections from "../components/sections/Collections";
-import Experience from "../components/sections/Experience";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="bg-black text-white">
-      <Header />
-      <Hero />
-      <Collections />
-      <Experience />
-    </main>
-  );
+  redirect("/tienda");
 }
